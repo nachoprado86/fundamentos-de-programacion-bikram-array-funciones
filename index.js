36f28a4213d18a23c4681adc12b02c13c81bd162
nachoprado86 <fonsvitae.spain@gmail.com>
2023-05-29 20:38:09 +0200

    first change

---
+++ index.js
@@ -0,0 +1,36 @@
+// ARREGLOS
+const arrayVacio = [];
+
+const arrayNumero = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const arrayNumerosPares = [0, 2, 4 ,6, 8];
+
+const arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']];
+
+// FUNCIONES
+function suma (a, b) {
+    return a + b;
+}
+
+function potencia (a, b) {
+    return a**b
+}
+const separarPalabras = (string) => {    
+    return string.split(" ");
+};
+
+const repetirString = (string,num) => {
+     return string.repeat (num);
+    
+}
+
+// Mezclando arreglos y funciones
+// arreglos
+const arrayNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9];
+
+cons
+// funciones
+// Mezclando arreglos y funciones
+
+
+// prueba
